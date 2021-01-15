57a13baf54adda6e01f7432234187466ce42e801
Markus Rudolph <markhoernchen@gmx.de>
2021-01-15 15:57:45 +0100

    Complete SVG paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./styles.css";
 import { useSize } from "./useSize";
 import { Stage } from "./Stage";
-import { Path, MoveTo, LineTo } from "./Paths";
+import { Path, Cmd_m, Cmd_t, Cmd_Z } from "./Paths";
 
 export default function App() {
   const size = useSize(() => ({
@@ -13,9 +13,12 @@ export default function App() {
   return (
     <Stage width={size.width} height={size.height}>
       <rect width={size.width} height={size.height} fill="blue" />
-      <Path>
-        <MoveTo x={100} y={100} />
-        <LineTo x={200} y={100} />
+      <Path fill="red" strokeWidth="20">
+        <Cmd_m dx={100} dy={100} />
+        <Cmd_t dx={100} dy={0} />
+        <Cmd_t dx={0} dy={100} />
+        <Cmd_t dx={-100} dy={0} />
+        <Cmd_t dx={0} dy={-100} />
       </Path>
     </Stage>
   );
--- src/Paths.tsx
+++ src/Paths.tsx
@@ -1,42 +1,145 @@
 import * as React from "react";
 import { FC, useEffect, useState } from "react";
 
-export enum Command {
-  ArcTo,
-  HorizontalLineTo,
-  VerticalLineTo,
-  Close,
-  Cubic,
-  SymetricCubic,
-  Quadratic,
-  SymetricQuadratic
+export interface XProps {
+  x: number;
 }
 
-export interface PathItemBase {
-  absolute?: boolean;
+export interface YProps {
+  y: number;
 }
 
-export interface MoveToProps extends PathItemBase {
+export interface XYProps extends XProps, YProps {}
+
+export interface DxProps {
+  dx: number;
+}
+
+export interface DyProps {
+  dy: number;
+}
+
+export interface CProps {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
   x: number;
   y: number;
 }
 
-export interface LineToProps extends PathItemBase {
+export interface cProps {
+  dx1: number;
+  dy1: number;
+  dx2: number;
+  dy2: number;
+  dx: number;
+  dy: number;
+}
+
+export interface SProps {
+  x2: number;
+  y2: number;
   x: number;
   y: number;
 }
 
-export const MoveTo: FC<MoveToProps> = () => <></>;
-export const LineTo: FC<LineToProps> = () => <></>;
+export interface sProps {
+  dx2: number;
+  dy2: number;
+  dx: number;
+  dy: number;
+}
+
+export interface QProps {
+  x1: number;
+  y1: number;
+  x: number;
+  y: number;
+}
+
+export interface qProps {
+  dx1: number;
+  dy1: number;
+  dx: number;
+  dy: number;
+}
+
+export interface DxDyProps extends DxProps, DyProps {}
+
+export interface ArcProps {
+  rx: number;
+  ry: number;
+  rotation: number;
+  largeArc: boolean;
+  sweep: boolean;
+}
+
+export interface AProps extends ArcProps, XYProps {}
+
+export interface aProps extends ArcProps, DxDyProps {}
+
+type Props =
+  | DxDyProps
+  | XYProps
+  | XProps
+  | YProps
+  | DxProps
+  | DyProps
+  | {}
+  | CProps
+  | cProps
+  | SProps
+  | sProps
+  | QProps
+  | qProps
+  | AProps
+  | aProps;
+
+export const Cmd_M: FC<XYProps> = () => <></>;
+export const Cmd_m: FC<DxDyProps> = () => <></>;
+export const Cmd_L: FC<XYProps> = () => <></>;
+export const Cmd_l: FC<DxDyProps> = () => <></>;
+
+export const Cmd_H: FC<XProps> = () => <></>;
+export const Cmd_h: FC<DxProps> = () => <></>;
+export const Cmd_V: FC<YProps> = () => <></>;
+export const Cmd_v: FC<DyProps> = () => <></>;
+
+export const Cmd_C: FC<CProps> = () => <></>;
+export const Cmd_c: FC<cProps> = () => <></>;
+
+export const Cmd_S: FC<SProps> = () => <></>;
+export const Cmd_s: FC<sProps> = () => <></>;
+
+export const Cmd_Q: FC<QProps> = () => <></>;
+export const Cmd_q: FC<qProps> = () => <></>;
+
+export const Cmd_T: FC<XYProps> = () => <></>;
+export const Cmd_t: FC<DxDyProps> = () => <></>;
+
+export const Cmd_A: FC<AProps> = () => <></>;
+export const Cmd_a: FC<aProps> = () => <></>;
+
+export const Cmd_Z: FC<{}> = () => <></>;
 
 interface Component {
   type: Function;
-  props: {};
+  props: Props;
 }
 
-export interface PathProps {}
+export interface PathProps {
+  fill?: string;
+  stroke?: string;
+  strokeWidth?: string;
+}
 
-export const Path: FC<PathProps> = ({ children }) => {
+export const Path: FC<PathProps> = ({
+  children,
+  fill = "transparent",
+  stroke = "black",
+  strokeWidth = "1px"
+}) => {
   const [data, setData] = useState(() => "");
 
   useEffect(() => {
@@ -44,17 +147,124 @@ export const Path: FC<PathProps> = ({ children }) => {
     React.Children.forEach(children, (child) => {
       const { type, props } = (child as unknown) as Component;
       switch (type) {
-        case MoveTo: {
-          const { x, y, absolute } = props as MoveToProps;
-          const m = absolute ? "M" : "m";
-          d.push(`${m} ${x} ${y}`);
+        case Cmd_M: {
+          const { x, y } = props as XYProps;
+          d.push(`M ${x} ${y}`);
           break;
         }
 
-        case LineTo: {
-          const { x, y, absolute } = props as LineToProps;
-          const l = absolute ? "L" : "l";
-          d.push(`${l} ${x} ${y}`);
+        case Cmd_m: {
+          const { dx, dy } = props as DxDyProps;
+          d.push(`m ${dx} ${dy}`);
+          break;
+        }
+
+        case Cmd_L: {
+          const { x, y } = props as XYProps;
+          d.push(`L ${x} ${y}`);
+          break;
+        }
+
+        case Cmd_l: {
+          const { dx, dy } = props as DxDyProps;
+          d.push(`l ${dx} ${dy}`);
+          break;
+        }
+
+        case Cmd_H: {
+          const { x } = props as XProps;
+          d.push(`H ${x}`);
+          break;
+        }
+
+        case Cmd_h: {
+          const { dx } = props as DxProps;
+          d.push(`h ${dx}`);
+          break;
+        }
+
+        case Cmd_V: {
+          const { y } = props as YProps;
+          d.push(`V ${y}`);
+          break;
+        }
+
+        case Cmd_v: {
+          const { dy } = props as DyProps;
+          d.push(`v ${dy}`);
+          break;
+        }
+
+        case Cmd_C: {
+          const { x1, y1, x2, y2, x, y } = props as CProps;
+          d.push(`C ${x1} ${y1}, ${x2} ${y2}, ${x} ${y}`);
+          break;
+        }
+
+        case Cmd_c: {
+          const { dx1, dy1, dx2, dy2, dx, dy } = props as cProps;
+          d.push(`c ${dx1} ${dy1}, ${dx2} ${dy2}, ${dx} ${dy}`);
+          break;
+        }
+
+        case Cmd_S: {
+          const { x2, y2, x, y } = props as SProps;
+          d.push(`S ${x2} ${y2}, ${x} ${y}`);
+          break;
+        }
+
+        case Cmd_s: {
+          const { dx2, dy2, dx, dy } = props as sProps;
+          d.push(`s ${dx2} ${dy2}, ${dx} ${dy}`);
+          break;
+        }
+
+        case Cmd_Q: {
+          const { x1, y1, x, y } = props as QProps;
+          d.push(`Q ${x1} ${y1}, ${x} ${y}`);
+          break;
+        }
+
+        case Cmd_q: {
+          const { dx1, dy1, dx, dy } = props as qProps;
+          d.push(`q ${dx1} ${dy1}, ${dx} ${dy}`);
+          break;
+        }
+
+        case Cmd_T: {
+          const { x, y } = props as XYProps;
+          d.push(`T ${x} ${y}`);
+          break;
+        }
+
+        case Cmd_t: {
+          const { dx, dy } = props as DxDyProps;
+          d.push(`t ${dx} ${dy}`);
+          break;
+        }
+
+        case Cmd_A: {
+          const { rx, ry, rotation, largeArc, sweep, x, y } = props as AProps;
+          d.push(
+            `A ${rx} ${ry} ${rotation} ${largeArc ? "1" : "0"} ${
+              sweep ? "1" : "0"
+            } ${x} ${y}`
+          );
+          break;
+        }
+
+        case Cmd_a: {
+          const { rx, ry, rotation, largeArc, sweep, dx, dy } = props as aProps;
+          d.push(
+            `A ${rx} ${ry} ${rotation} ${largeArc ? "1" : "0"} ${
+              sweep ? "1" : "0"
+            } ${dx} ${dy}`
+          );
+          break;
+        }
+
+        case Cmd_Z: {
+          d.push(`Z`);
           break;
         }
       }
@@ -62,5 +272,7 @@ export const Path: FC<PathProps> = ({ children }) => {
     setData(d.join(" "));
   }, [children]);
 
-  return <path d={data} stroke="black" />;
+  return (
+    <path d={data} strokeWidth={strokeWidth} stroke={stroke} fill={fill} />
+  );
 };
