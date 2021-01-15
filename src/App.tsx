776369f5518ec0cbc857f51c812fbe7f7b40baf0
Markus Rudolph <markhoernchen@gmx.de>
2021-01-15 10:38:06 +0100

    Add lines

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import "./styles.css";
 import { useSize } from "./useSize";
 import { Stage } from "./Stage";
+import { Path, MoveTo, LineTo } from "./Paths";
 
 export default function App() {
   const size = useSize(() => ({
@@ -12,6 +13,10 @@ export default function App() {
   return (
     <Stage width={size.width} height={size.height}>
       <rect width={size.width} height={size.height} fill="blue" />
+      <Path>
+        <MoveTo x={100} y={100} />
+        <LineTo x={200} y={100} />
+      </Path>
     </Stage>
   );
 }
--- src/Paths.tsx
+++ src/Paths.tsx
@@ -1,3 +1,66 @@
-//import * as React from "react";
+import * as React from "react";
+import { FC, useEffect, useState } from "react";
 
-//export const Path
+export enum Command {
+  ArcTo,
+  HorizontalLineTo,
+  VerticalLineTo,
+  Close,
+  Cubic,
+  SymetricCubic,
+  Quadratic,
+  SymetricQuadratic
+}
+
+export interface PathItemBase {
+  absolute?: boolean;
+}
+
+export interface MoveToProps extends PathItemBase {
+  x: number;
+  y: number;
+}
+
+export interface LineToProps extends PathItemBase {
+  x: number;
+  y: number;
+}
+
+export const MoveTo: FC<MoveToProps> = () => <></>;
+export const LineTo: FC<LineToProps> = () => <></>;
+
+interface Component {
+  type: Function;
+  props: {};
+}
+
+export interface PathProps {}
+
+export const Path: FC<PathProps> = ({ children }) => {
+  const [data, setData] = useState(() => "");
+
+  useEffect(() => {
+    let d: string[] = [];
+    React.Children.forEach(children, (child) => {
+      const { type, props } = (child as unknown) as Component;
+      switch (type) {
+        case MoveTo: {
+          const { x, y, absolute } = props as MoveToProps;
+          const m = absolute ? "M" : "m";
+          d.push(`${m} ${x} ${y}`);
+          break;
+        }
+
+        case LineTo: {
+          const { x, y, absolute } = props as LineToProps;
+          const l = absolute ? "L" : "l";
+          d.push(`${l} ${x} ${y}`);
+          break;
+        }
+      }
+    });
+    setData(d.join(" "));
+  }, [children]);
+
+  return <path d={data} stroke="black" />;
+};
